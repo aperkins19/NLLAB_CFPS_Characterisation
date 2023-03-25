ab2d4ffc10e006ebdaea36733c151edda2a4f69a
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2023-03-25 15:23:52 +0000

    finished labstep annotation

--- cfps_data_analysis/scripts/2_labstep_annotation.py
+++ cfps_data_analysis/scripts/2_labstep_annotation.py
@@ -1,8 +1,8 @@
 """
 # Intro
 
-First script in the data preprocessing that:
-* Takes in the raw plate reader csv / excel file and well metadata file
+2nd script in the data preprocessing that:
+* Annotates the tidy dataset with metadata from lysate manufacture via the labstep API
 * Returns Tidy Dataset annotated with the metadata.
 """
 
@@ -20,6 +20,9 @@ paths = json.load(open("/data_pipeline_app/config/paths.json"))
 # get api_authentication
 labstep_api_auth = json.load(open(paths["API_Authentication"]))
 
+# get lysate data categories
+lysate_data_categories = json.load(open(paths["Input"]["Lysate_Data_Categories"]))
+
 """
 Login to Labstep API
 """
@@ -30,4 +33,58 @@ SBSG_Workspace, user = AuthenticateByUserApiKey(
     workspace_id = labstep_api_auth["Labstep_API"]["Workspace_ID"]
     )
 
-print(user)
+"""
+Import the tidy data
+Extract unique lysates
+"""
+
+tidy_data = pd.read_csv(paths["Output"]["Datasets"]+"tidy_data.csv")
+
+unique_lysates = tidy_data["Lysate_Inventory_Record"].unique()
+
+"""
+Look up the lysates data via the labstep API
+"""
+
+# Look up the lysates data via the labstep API and store in list
+
+# initialise list
+lysate_data_series_list = []
+# interate over the lysates
+for lysate in unique_lysates:
+
+    # extract the lysate data and append to list
+    # functions in labstep_utils.py
+    lysate_data_series_list.append(
+        GetLysateData(
+            lysate,
+            lysate_data_categories,
+            SBSG_Workspace
+            )
+        )
+
+# iterate over the rows in tidy data
+new_row_list = []
+
+for i, row in tidy_data.iterrows():
+
+    # iterate over the lysate data series list
+    for lysate_data_series in lysate_data_series_list:
+
+        # match the Lysate_Inventory_Records
+        if row["Lysate_Inventory_Record"] == lysate_data_series["Lysate_Inventory_Record"]:
+
+            # drop Lysate_Inventory_Record to avoid duplication
+            row = row.drop(["Lysate_Inventory_Record"])
+
+            # join the two series, convert to a DF and transpose to make the index the columns
+            new_row = pd.concat([row, lysate_data_series], axis = 0).to_frame().transpose()
+
+            # add the 1D DF to the list 
+            new_row_list.append(new_row)
+
+# construct the new df
+tidy_data_labstep_annotated = pd.concat(new_row_list, axis=0).reset_index(drop=True)
+
+# save to disk
+tidy_data_labstep_annotated.to_csv(paths["Output"]["Datasets"]+"tidy_data_labstep_annotated.csv", index=False)
--- cfps_data_analysis/scripts/analysis_functions/labstep_utils.py
+++ cfps_data_analysis/scripts/analysis_functions/labstep_utils.py
@@ -11,16 +11,78 @@ def AuthenticateByUserApiKey(email, apiKey, workspace_id):
     1. logs in user
     2. returns workspace
     """
-
     # logs in
     user = labstep.authenticate(
         email,
         apiKey
         )
-    
     # gets workspace.
     workspace = user.getWorkspace(
             workspace_id = workspace_id
             )
     
     return workspace, user
+
+def WrangleLabstepDataFields(lysate_data, lysate_data_categories):
+
+    """
+    Iterates over the lysate_data_categories to look up data values in the experiement
+    associated with the lysate item.
+    Extracts and returns data as a pd Series.
+    """
+    # initialise dict
+    retrieved_data_dict = {}
+
+    # iterate over the categories
+    for category in lysate_data_categories.keys():
+
+        # extracts the data
+        retrieved_data = lysate_data.get(category)
+        # populates the dictionary
+        retrieved_data_dict[category] = retrieved_data.getValue()
+    # returns series
+    return pd.Series(retrieved_data_dict)
+
+
+
+def GetLysateData(lysate_name, lysate_data_categories, workspace):
+
+    # retrieve the lysate resource record and access the item.
+    # Note, there should be only one item.
+
+    # as this returns a list and there should only be one lysate with that name,
+    # a sanity check is performed.
+    lysate = workspace.getResources(
+    search_query = lysate_name
+    )
+
+    if len(lysate) > 1:
+        raise Exception("There appears to be more than one lysate in the inventory with the name: " + lysate_name + ". Please resolve before proceeding.")
+    elif len(lysate) == 0:
+        raise Exception("There appears no lysates in the inventory with the name: " + lysate_name + ". Please resolve before proceeding.")
+    else:
+        # if theres only one, extract
+        lysate = lysate[0]
+
+    # extract the item for that lysate and repeat sanity check.
+    lysate = lysate.getItems()
+    if len(lysate) > 1:
+        raise Exception("There appears to be more than one item associated with " + lysate_name + ". Please resolve before proceeding.")
+    elif len(lysate) == 0:
+        raise Exception("There appears no items associated with " + lysate_name + ". Please resolve before proceeding.")
+    else:
+        # if theres only one, extract
+        lysate = lysate[0]
+
+    # extract the data associated with that lysate
+    lysate_data = lysate.getData()
+
+    # function defined above
+    # returns pd Series
+    lysate_data_series = WrangleLabstepDataFields(lysate_data, lysate_data_categories)
+
+    # append lysate name to make matching unambigious
+    lysate_data_series["Lysate_Inventory_Record"] = lysate_name
+
+    return lysate_data_series
+
