dad9f423cf4761571053c2f20e7febfbca542e44
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2023-03-26 18:40:13 +0100

    made paths absolute for front end access

--- DataDashboard/cfps_data_analysis/preprocessing.bash
+++ DataDashboard/cfps_data_analysis/preprocessing.bash
@@ -3,15 +3,15 @@
 # this script automates the raw cfps timeseries fluorescence data pipeline
 
 # delete and reinitalise ./output
-rm -r output
+rm -r /DataDashboard_app/cfps_data_analysis/output/
 
-mkdir output
-mkdir output/datasets
-mkdir output/tmp
+mkdir /DataDashboard_app/cfps_data_analysis/output/
+mkdir /DataDashboard_app/cfps_data_analysis/output/datasets
+mkdir /DataDashboard_app/cfps_data_analysis/output/tmp
 
 
 # run scripts
-python3 "scripts/1_preprocessing_tidy.py"
+python3 "/DataDashboard_app/cfps_data_analysis/scripts/1_preprocessing_tidy.py"
 
-python3 "scripts/2_labstep_annotation.py"
+python3 "/DataDashboard_app/cfps_data_analysis/scripts/2_labstep_annotation.py"
 
