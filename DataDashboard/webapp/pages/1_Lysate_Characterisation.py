26c74d92e693dcbd6fc9d13f65ab339e0d31bf88
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2023-03-26 23:57:03 +0100

    lysate charactisation nearly complete

--- DataDashboard/webapp/pages/1_Lysate_Characterisation.py
+++ DataDashboard/webapp/pages/1_Lysate_Characterisation.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import pandas as pd
 import json
-import os
-import subprocess
-from subprocess import call
-import sys
+
 
 #from utils.lysate_charactisation_utils import *
 from utils.bash import *
+from utils.labstep_utils import *
+
+import os
 
 # import paths
 paths = json.load(open("/DataDashboard_app/cfps_data_analysis/config/paths.json"))
@@ -34,28 +34,58 @@ def convert_df(df):
 
 #######
 # initialise session states for buttons and cached files
+if "raw_data_viewer_expanded" not in st.session_state:
+    st.session_state["raw_data_viewer_expanded"] = True
+
+if "form_expanded" not in st.session_state:
+    st.session_state["form_expanded"] = True
+
 if "raw_file_cached_status" not in st.session_state:
     st.session_state["raw_file_cached_status"] = False
 
-if "raw_upload_button" not in st.session_state:
-    st.session_state["raw_upload_button"] = False
+if "form_submitted" not in st.session_state:
+    st.session_state["form_submitted"] = False
 
-if "run_preprocessing_button" not in st.session_state:
-    st.session_state["run_preprocessing_button"] = False
+if "data_preprocessing_complete" not in st.session_state:
+    st.session_state["data_preprocessing_complete"] = False
+
+if "Calibration_Model_Selected" not in st.session_state:
+    st.session_state["Calibration_Model_Selected"] = "None"
+
+if "Calibration_Models_Available" not in st.session_state:
+    # initialise the list
+    st.session_state["Calibration_Models_Available"] = []
+    st.session_state["Calibration_Models_Available"] = st.session_state["Calibration_Models_Available"] = ["None"]
+    # read the directory and remove the file extensions
+    for model in os.listdir(paths["Calibration"]["Models"]):
+        st.session_state["Calibration_Models_Available"] = st.session_state["Calibration_Models_Available"] + [model[:-4]]
+
+if "Lysate_Inventory_List" not in st.session_state:
+    # midly hacky way to import the json storing the lysates found in the database
+    st.session_state["Lysate_Inventory_List"] = []
+    for lysate in list(json.load(open("/DataDashboard_app/webapp/utils/lysate_list.json")).values()):
+        st.session_state["Lysate_Inventory_List"] = st.session_state["Lysate_Inventory_List"] + lysate
+
+if "Lysate_selected" not in st.session_state:
+    st.session_state["Lysate_selected"] = "None"   
 
 # define callbacks for updating button click session states
-def raw_upload_button_callback():
-    st.session_state["raw_upload_button"] = True
-    # resets run preprocessing session state
-    st.session_state["run_preprocessing_button"] = False
-
-def run_preprocessing_button_callback():
-    st.session_state["run_preprocessing_button"] = True
-
 def raw_file_cached_status_callback():
     st.session_state["raw_file_cached_status"] = True
 
 
+def form_callback():
+    # saves form information
+    st.session_state["Calibration_Model_Selected"] = calibration_model_selected
+    st.session_state["Lysate_selected"] = lysate_selected
+    # sets button states
+    st.session_state["form_submitted"] = True
+    # minimise forms
+    st.session_state["raw_data_viewer_expanded"] = False
+    st.session_state["form_expanded"] = False
+
+
+#### begin page
 
 st.title("Lysate Characterisation")
 st.write("text here to explain")
@@ -86,52 +116,99 @@ if (st.session_state["raw_file_cached_status"]
     st.markdown("Please check that the file looks like it should :mag:")
 
      # display df
-    raw_data_df_expander =  st.expander("Show", expanded=True)
+    raw_data_df_expander =  st.expander("Show", expanded= st.session_state["raw_data_viewer_expanded"])
     raw_data_df_expander.write(raw_upload)
+
 
     st.subheader("Enter Labstep stuff")
 
-    meta_data_form =  st.expander("Show", expanded=True)
-
-    meta_data_form.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")
-    meta_data_form.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
-
-    # display button for save
-
-    if (
-        st.button("Upload", on_click = raw_upload_button_callback)
-        or
-        st.session_state["raw_upload_button"]
-        ):
+    meta_data_form =  st.expander("Show", expanded=st.session_state["form_expanded"])
 
 
-        # write excel file to server input directory
-        raw_upload.to_excel(paths["Input"]["Raw_Data"]+"raw.xlsx", index=False, header=False)
+    with st.form(key='meta_data_form'):
+        
+        col1, col2 = meta_data_form.columns(2)
 
-
-        if (
-            st.button("Run Preprocessing", on_click = run_preprocessing_button_callback)
-            and
-            st.session_state["raw_upload_button"]
-            ):
-
-            
-            run_preprocessing_bash_script()
-
-            processed_df = read_processed_csv(paths["Output"]["Datasets"]+"tidy_data_labstep_annotated.csv")
-            
-            st.write(processed_df)
-
-            csv = convert_df(processed_df)
-
-            st.download_button(
-                label="Download data as CSV",
-                data=csv,
-                file_name='large_df.csv',
-                mime='text/csv',
+        with col1:
+            # calibration model selection
+            calibration_model_selected = st.selectbox(
+                "Select a Calibration Model:",
+                # retrieves the models from session state
+                (st.session_state["Calibration_Models_Available"])
             )
 
+        with col2:
+
+            # lysate selection
+
+            lysate_selected = st.selectbox(
+                "Select the Lysate from the Labstep Inventory:",
+                # retrieves the lysates from session state
+                (st.session_state["Lysate_Inventory_List"])
+                )
             
+            # Refresh button
+            st.write("Don't see your lysate? Try refreshing the list:")
+
+            # button initialisation
+            refresh_lysates = st.button("Refresh Lysate List")
+
+            # on call
+            if refresh_lysates:
+                # wait message
+                st.info('Refreshing Lysate List by querying Labstep Database. This may take a few seconds..', icon="ℹ️")
+                # execute python script
+                s = subprocess.run(["python3", "DataDashboard_app/webapp/utils/search_lysates_in_api.py"])
+                st.success('Lysates refreshed. Reload the page to see them.', icon="✅")
+
+        # wait for both to be selected before displaying submit button
+        if calibration_model_selected != "None" and lysate_selected != "None":
+            if (
+                st.form_submit_button("Submit", on_click = form_callback)
+                or
+                st.session_state["form_submitted"]
+                ):
+
+                #st.write(st.session_state)
+
+                #### write metadata collected to disk before running preprocessing
+
+                # Analysis_Config
+                Analysis_Config = {
+                    "Calibration_Model": st.session_state["Calibration_Model_Selected"]
+                }
+                ##### write analysis config json here
+                with open(paths["Input"]["Analysis_Config"], 'w') as fp:
+                    json.dump(Analysis_Config, fp)
+
+
+                ##### write well_metadata json here
+
+                # write excel file to server input directory
+                raw_upload.to_excel(paths["Input"]["Raw_Data"]+"raw.xlsx", index=False, header=False)
+
+                run_preprocessing_bash_script()
+
+                processed_df = read_processed_csv(paths["Output"]["Datasets"]+"tidy_data_labstep_annotated.csv")
+                
+                st.write(processed_df)
+
+                st.session_state["data_preprocessing_complete"] = True
+
+    if (
+        st.session_state["data_preprocessing_complete"]
+        ):
+
+        csv = convert_df(processed_df)
+
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name= st.session_state["Lysate_selected"] + '_tidy_annotated_timecourse_data.csv',
+            mime='text/csv',
+        )
+
+        st.line_chart(data=processed_df, x="Time", y="GFP_uM", width=0, height=0, use_container_width=True)
 
 
 
