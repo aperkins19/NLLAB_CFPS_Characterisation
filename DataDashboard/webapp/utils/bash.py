698d8388ae5189f452985ac2e6a100f3cd86d1ed
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2023-03-26 18:41:41 +0100

    added front end utils

---
+++ DataDashboard/webapp/utils/bash.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import subprocess
+
+
+
+""""
+test
+"""
+def run_preprocessing_bash_script():
+
+    p = subprocess.run(
+                ["/bin/bash", "DataDashboard_app/cfps_data_analysis/preprocessing.bash"],
+                executable="/bin/bash"
+    )
+    return st.success('Data Preprocessing complete.', icon="✅")
+
