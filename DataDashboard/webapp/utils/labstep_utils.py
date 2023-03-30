7e1fcb42b676feeba7dfc8ba22047d27e3ed2be2
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2023-03-30 01:14:02 +0100

    consolidated labstep functionalities

--- DataDashboard/webapp/utils/labstep_utils.py
+++ DataDashboard/webapp/utils/labstep_utils.py
@@ -20,7 +20,10 @@ def AuthenticateByUserApiKey(email, apiKey, workspace_id):
     return workspace, user
 
 @st.cache_data
-def Labstep_Login(labstep_api_auth):
+def Labstep_Login(paths):
+
+    # get api_authentication
+    labstep_api_auth = json.load(open(paths["API_Authentication"]))
 
     SBSG_Workspace, user = AuthenticateByUserApiKey(
         email = labstep_api_auth["Labstep_API"]["Email"],
@@ -31,21 +34,49 @@ def Labstep_Login(labstep_api_auth):
     return SBSG_Workspace, user
 
 
-#lysate_name_prefix = "Lysate_"
-#lysate = workspace.getResources(
-#search_query = lysate_name
-#)
-""" lysate_inventory_list = []
-lysate_name_prefix = "Lysate_"
-for lysate_number in range(0,100, 1):
-    num = str(int(lysate_number) + 1).zfill(3)
-    lysate_name = lysate_name_prefix+num
 
-    lysate = SBSG_Workspace.getResources(
-    search_query = lysate_name
-    )
+def SearchForLysates(paths):
 
-    if len(lysate) != 0:
-        lysate_inventory_list.append(lysate_name)
+    SBSG_Workspace, user = Labstep_Login(paths)
 
-st.write(lysate_inventory_list) """
+    lysate_inventory_list = []
+    lysate_name_prefix = "Lysate_"
+
+    # define the break counter
+    # breaks out of loop if 5 consecutive empty records are returned
+    break_counter = 0
+
+    # increment the lysate number
+    for lysate_number in range(0,100, 1):
+        # add zeros infront
+        num = str(int(lysate_number) + 1).zfill(3)
+        # build complete name
+        lysate_name = lysate_name_prefix+num
+        # search api for that lysate name
+        lysate = SBSG_Workspace.getResources(
+        search_query = lysate_name
+        )
+        # if is not an empty list, append name to list
+        if len(lysate) != 0:
+            lysate_inventory_list.append(lysate_name)
+        elif len(lysate) == 0:
+            break_counter = break_counter + 1
+
+        if break_counter > 5:
+            break
+
+
+    st.write(lysate_inventory_list)
+    # on completion
+    # stick None on the begining so that it shows up in the selection bar
+    lysate_inventory_list.insert(0, "None")
+    # reverse list so that the latest shows up first
+    lysate_inventory_list.reverse()
+    # build dictionary and save as json.
+    to_save_json = {
+        "Lysate_Inventory_List": lysate_inventory_list
+    }
+    # write to disk for it to be read in on session start
+    with open(paths["Lysate_Inventory_List"], 'w') as fp:
+        json.dump(to_save_json, fp)
+
